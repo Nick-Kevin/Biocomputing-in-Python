ede2db1a3cfa2efa27bb3fd805cfa3f6dbc59253
Nick-Kevin <nicblou23@gmail.com>
2024-01-06 13:52:07 +0300

    update euler_method

--- euler_method.py
+++ euler_method.py
@@ -19,3 +19,37 @@ def euler(interval, subintervals, coefficients, coefficients_number):
     for i in x:
         print(i)
     return x[len(x)-1]
+
+def main():
+    NumberOfCoefficients = int(input("\nFor the f(x,t) function:\nEnter the number of coefficients: "))
+    CoefficientsOfTheFunction = []
+    Degree = NumberOfCoefficients - 1
+    for i in range(NumberOfCoefficients):
+        Coefficient = int(input("Enter the coefficient for x^"+str(Degree)+": "))
+        CoefficientsOfTheFunction.append(Coefficient)
+        Degree -= 1
+    print()
+    Degree = NumberOfCoefficients - 1
+    TheFunction = ""
+    for coefficient in CoefficientsOfTheFunction:
+        if Degree == 0:
+            if coefficient != 0:
+                TheFunction += str(coefficient)
+        elif Degree == 1:
+            if coefficient == 1:
+                TheFunction += "x + "
+            elif coefficient == 0:
+                TheFunction += ""
+            else:
+                TheFunction += str(coefficient) + "x + "
+        else:
+            if coefficient == 1:
+                TheFunction += "x^" + str(Degree) + " + "
+            elif coefficient == 0:
+                TheFunction += ""
+            else:
+                TheFunction += str(coefficient) + "x^" + str(Degree) + " + "
+        Degree -= 1
+    print("So f(x,t) =", TheFunction)
+
+main()
