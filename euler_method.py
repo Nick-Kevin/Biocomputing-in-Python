d9b9f6a7a48b167c29c8dfa2ab63c30e636e7e48
Nick-Kevin <nicblou23@gmail.com>
2024-01-06 14:17:34 +0300

    update euler method

--- euler_method.py
+++ euler_method.py
@@ -7,14 +7,15 @@ def calculate_reccurence_step(interval, subintervals):
     h = (b-a)/n
     return h
 
-def euler(interval, subintervals, coefficients, coefficients_number):
+def euler(interval, subintervals, coefficients):
     a = interval[0]
     n = subintervals
     x = []
     x.append(a)
     h = calculate_reccurence_step(interval, subintervals)
+    CoefficientsNumber = len(coefficients)
     for k in range(n):
-        NewX = x[k] + h*horner(coefficients, coefficients_number, x[k])
+        NewX = x[k] + h*horner(coefficients, CoefficientsNumber, x[k])
         x.append(NewX)
     for i in x:
         print(i)
@@ -27,8 +28,18 @@ def main():
     for i in range(NumberOfCoefficients):
         Coefficient = int(input("Enter the coefficient for x^"+str(Degree)+": "))
         CoefficientsOfTheFunction.append(Coefficient)
-        Degree -= 1
+        Degree -= 1    
+    print("For the [a,b] interval")
+    Interval = []
+    a = int(input("Enter a: "))
+    Interval.append(a)
+    b = int(input("Enter b: "))
+    Interval.append(b)
+    SubintervalsNumber = int(input("Enter the number of subintervals: "))
     print()
     display_function(NumberOfCoefficients, CoefficientsOfTheFunction)
+    print("The [a,b] interval is:", Interval)
+    print("The number of subintervals n is:", SubintervalsNumber)
+    euler(Interval, SubintervalsNumber, CoefficientsOfTheFunction)
 
 main()
